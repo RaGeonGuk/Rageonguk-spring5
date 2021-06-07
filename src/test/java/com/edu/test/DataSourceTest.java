be0b6257fcc7d689b833aa810f7bcac3c4e77800
라건국의 DeskTop < DeskTop@192.168.219.104>
2021-06-07 17:41:06 +0900

    월요일 종료

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -35,11 +35,23 @@ public class DataSourceTest {
 	DataSource dataSource;//Inject로 객체를 만들면 메모리 관리를 스프링이 대신해 줌.
 	//Inject 자바8부터 지원, 그럼, 이전 자바7에서 @Autowired 로 객체를 만들었슴
 	
+	//스프링 코딩 시작 순서
+	// M-V-C 사이에 데이터를 임시저장하는 공간(VO클래스-멤버변수 + Get/Set 메서드) 생성
+	// 보통 ValueObject 클래스는 DB테이블과 1:1로 매칭이됩니다.
+	// 그래서, 1. MemberVO.java VO클래스를  생성.
+	//2.  DB(마이바티스)쿼리를 만듭니다. (VO사용됨) - 내일부터 시작
+	@Test
+	public void selectMember() throws Exception {
+		// 회원관리 테이블에서 더미로 입력한 100개의 레코드를 출력 메서드 테스트 -> 회원관리 목록이 출력
+		// 스프링 코딩 순서
+	}
+	
+	
 	@Test
 	public void oldQueryTest () throws Exception {
 		//스프링빈을 사용하지 않을 때 예전 방식: 코딩테스트에서는 스프링설정을 안쓰고, 직접 DB 아이디/암호 입력
 		Connection connection = null;
-		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE","apmsetup");
+		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE2","apmsetup");
 		logger.debug("데이터베이스 직접 접속이 성공 하였습니다. DB종류는 "+ connection.getMetaData().getDatabaseProductName());
 		//직접 쿼리를 날립니다. 날리기전 쿼리문자 statement
 		Statement stmt = connection.createStatement();
