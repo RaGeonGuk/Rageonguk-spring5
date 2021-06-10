b7d3ef29104385da5a21ecf62b65f2bcb1bf3a05
User <User@DESKTOP-HOHL5IM>
2021-06-10 16:34:25 +0900

    7교시 중간 커밋

--- src/main/java/com/edu/controller/AdminController.java
+++ src/main/java/com/edu/controller/AdminController.java
@@ -1,9 +1,19 @@
 package com.edu.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.edu.service.IF_MemberService;
+import com.edu.vo.MemberVO;
+import com.edu.vo.PageVO;
 
 /**
  * 이 클래스는 Admin관리자단을 접근하는 클래스
@@ -14,11 +24,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class AdminController {
+	//컨트롤러 수정하면 자동로딩(auto 컴파일)
+	// 디버그용 로그객체 생성(아래)
+	private Logger logger = LoggerFactory.getLogger(AdminController.class);
 	// 이 메서드는 회원목록을 출력하는 jsp와 매핑이 됩니다.
-	@RequestMapping(value="/admin/board/board_list", method=RequestMethod.GET)
-	public String selecetmember () throws Exception {
-		
-		return "admin/board/board_list"; //jsp파일 상대경로;
+	@Inject
+	private IF_MemberService memberService;
+	
+	@RequestMapping(value="/admin/member/member_list", method=RequestMethod.GET)
+	public String selecetmember (PageVO pageVO) throws Exception {
+		//jsp의 검색시 serch_type, serarch_keywoard 내용이 PageVO클래스에 Set됩니다.
+		logger.info("디버그"+ pageVO.toString());//지금까지 jsp -> 컨트롤러 일방향 자료이동.
+		//위에서 검색어를 받아서 역방향 검색한 결과를jsp보내줍니다.(아래)
+		if (pageVO.getPage() == null) {
+			pageVO.setPage(1);
+		}
+		//pageVO의 calcPage메서드를 실행하려면, 필수 변수값 입력(아래)
+		pageVO.setQueryPerPageNum(10);
+		logger.info("디버그" + pageVO.toString());
+		List<MemberVO> listMember = memberService.selectMember(pageVO);
+		pageVO.setTotalCount(listMember.size());
+		return "admin/member/member_list"; //jsp파일 상대경로;
 	}
 	// URL요청 경로는 @RequestMappingt 반드시 절대경로로 표시
 	@RequestMapping(value="/admin", method=RequestMethod.GET)
