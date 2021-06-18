0bac1b6b273b728656133e9ebf0155946035f8d0
User <User@DESKTOP-HOHL5IM>
2021-06-18 11:30:26 +0900

    3교시 커밋 1

--- src/main/java/com/edu/vo/PageVO.java
+++ src/main/java/com/edu/vo/PageVO.java
@@ -8,6 +8,7 @@ package com.edu.vo;
  * 검색에 사용되는 변수(쿼리변수만): 검색어(search_keyword), 검색 조건(search_type)
  */
 public class PageVO {
+	private String board_type;//게시판 종류를 표시하는 전용 변수 추가
 	private int queryStartNo; //쿼리전용변수, 페이징쿼리에서 시작 페이지 인덱스번호를 표시하는 변수
 	private int queryPerPageNum; //쿼리전용, 페이징쿼리에서 1페이지당 출력할 내용을 표시하는 변수
 	private Integer page; // jsp에서 발생 선택한 페이지 번호 변수. 자바전용. int인데  Null값을 허용
@@ -19,8 +20,7 @@ public class PageVO {
 	private boolean next; //UI 하단 다음페이지로 이동이 가능한지 판별하는 변수
 	private String search_keyword; //jsp에서 받은 검색어 쿼리 전용 변수
 	private String search_type; //검색조건에 해당 쿼리전용 변수
-	
-		
+
 	
 	@Override
 	public String toString() {
@@ -28,6 +28,14 @@ public class PageVO {
 				+ ", perPageNum=" + perPageNum + ", totalCount=" + totalCount + ", startPage=" + startPage
 				+ ", endPage=" + endPage + ", prev=" + prev + ", next=" + next + ", search_keyword=" + search_keyword
 				+ ", search_type=" + search_type + "]";
+	}
+	//Get,Set용 메서드 추가
+	public String getBoard_type() {
+		return board_type;
+	}
+
+	public void setBoard_type(String board_type) {
+		this.board_type = board_type;
 	}
 
 	public int getQueryStartNo() {
