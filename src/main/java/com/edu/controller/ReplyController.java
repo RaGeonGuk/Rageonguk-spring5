2464ee9f5ae519eae9146149c0777a37d8574cbf
라건국의 DeskTop < DeskTop@192.168.219.119>
2021-06-29 16:51:43 +0900

    ㄱ

--- src/main/java/com/edu/controller/ReplyController.java
+++ src/main/java/com/edu/controller/ReplyController.java
@@ -33,19 +33,34 @@ public class ReplyController {
 	@Inject
 	private IF_ReplyService replyService;
 	
-	//댓글은 Read가 필요없음. 왜냐면 Select로 가져온 값을 Ajax로 처리하기 때문에 쿼리를 날릴 필요가 없습니다.
-	//그래서, 바로 Update를 처리합니다. -간단하게 update시 Read 쿼리가 없고, Ajax처리함
-	@RequestMapping(value="reply/reply_update", method=RequestMethod.PATCH)
-	public ResponseEntity<String> reply_update(@RequestBody ReplyVO replyVO) {
-		//@RequestBody jsp에서 $.ajax로 보내는 데이터값 <-> @ResponseBody		
+	//댓글 삭제를 RestFul로 처리
+	@RequestMapping(value="/reply/reply_delete/{bno}/{rno}", method=RequestMethod.DELETE)
+	public ResponseEntity<String> reply_delete(@PathVariable("bno")Integer bno,@PathVariable("rno")Integer rno) {
 		ResponseEntity<String> result = null;
+		ReplyVO replyVO = new ReplyVO();
+		replyVO.setBno(bno);
+		replyVO.setRno(rno);
 		try {
-			replyService.updateReply(replyVO);
-			result= new ResponseEntity<String>("success",HttpStatus.OK)
+			replyService.deleteReply(replyVO);
+			result = new ResponseEntity<String>("success",HttpStatus.OK);
 		} catch (Exception e) {
 			result = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		return result; //Restful 방식은 항상 body 반환값이(Body:String, Hasmap-json부분) 존재합니다.
+		return result;
+	}
+	//댓글은 Read가 필요없음. 왜냐하면, Select로 가져온 값을 Ajax로 처리하기 때문에 쿼리를 날릴 필요가 없습니다.
+	//그래서, 바로 Update를 처리합니다.-간단하게 update시 Read쿼리가 없고, Ajax처리함.
+	@RequestMapping(value="/reply/reply_update", method=RequestMethod.PATCH)
+	public ResponseEntity<String> reply_update(@RequestBody ReplyVO replyVO) {
+		//@RequestBody jsp에서 $.ajax를 이용해서 보내는 데이터값 <-> @ResponseBody 응답return값
+		ResponseEntity<String> result = null;
+		try {
+			replyService.updateReply(replyVO);
+			result = new ResponseEntity<String>("success",HttpStatus.OK);
+		} catch (Exception e) {
+			result = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return result;//Restful방식은 항상 반환값이(Body:String, Hasmap-json부분) 존재합니다.
 	}
 	//댓글 등록 @RequestBody는 jsp에서 Ajax메서드로 보내온 값을 받을때 사용하는 애노테이션 입니다.
 	@RequestMapping(value="/reply/reply_insert", method=RequestMethod.POST)
@@ -59,7 +74,7 @@ public class ReplyController {
 			result = new ResponseEntity<String>("success",HttpStatus.OK);//객체 생성시 매개변수로 상태값 + 입력성공시 success라는 문자열도 보냄
 			
 		} catch (Exception e) {
-			result = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+			result = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
 		return result;
